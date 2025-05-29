abd26af9c68cbaec1b7efe13e3510f3a755e54ee
pangeding <15533858+pangeding@user.noreply.gitee.com>
2025-05-29 20:31:21 +0800

    登录功能

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,80 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public  LoginInterceptor(StringRedisTemplate stringRedisTemplate){
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1. 获取session
+        //HttpSession session = request.getSession();
+        //2. 获取session中的用户
+        //Object user = session.getAttribute("user");
+
+        //3. 判断用户是否存在
+        /*if(user == null){
+            //4. 不存在，拦截，返回401状态码
+            response.setStatus(401);
+            return false;
+        }*/
+        //5. 存在，保存用户信息到threadlocal
+        //UserHolder.saveUser((UserDTO) user);
+        //6. 放行
+
+        //1. 获取请求头里的token
+        String token = request.getHeader("authorization");
+        if(StrUtil.isBlank(token)){
+            //4. 不存在，拦截，返回401状态码
+            response.setStatus(401);
+            return false;
+        }
+
+        //2.基于token获取redis中的用户
+        String key = RedisConstants.LOGIN_USER_KEY + token;
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash()
+                .entries(key);
+        //3. 判断用户是否存在
+        if(userMap.isEmpty()){
+            //4. 不存在，拦截，返回401状态码
+            response.setStatus(401);
+            return false;
+        }
+
+        //5. 将查询到的hash数据转化为userDTO对象
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+
+        //6. 存在，保存用户信息到threadlocal
+        UserHolder.saveUser(userDTO);
+
+        //7. 刷新token有效期
+        stringRedisTemplate.expire(key, LOGIN_USER_TTL, TimeUnit.MINUTES);
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
