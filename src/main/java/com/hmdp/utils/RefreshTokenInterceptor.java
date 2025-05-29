6aa3ab43f31b9655c9e80f1e99dc869874626e69
pangeding <15533858+pangeding@user.noreply.gitee.com>
2025-05-29 22:36:50 +0800

    刷新token拦截器

--- src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -3,27 +3,24 @@ package com.hmdp.utils;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
 
-public class LoginInterceptor implements HandlerInterceptor {
+public class RefreshTokenInterceptor implements HandlerInterceptor {
 
-    /*private StringRedisTemplate stringRedisTemplate;
+    private StringRedisTemplate stringRedisTemplate;
 
-    public  LoginInterceptor(StringRedisTemplate stringRedisTemplate){
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate){
         this.stringRedisTemplate = stringRedisTemplate;
-    }*/
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -42,12 +39,13 @@ public class LoginInterceptor implements HandlerInterceptor {
         //UserHolder.saveUser((UserDTO) user);
         //6. 放行
 
-        /*//1. 获取请求头里的token
+        //1. 获取请求头里的token
         String token = request.getHeader("authorization");
         if(StrUtil.isBlank(token)){
-            //4. 不存在，拦截，返回401状态码
+            /*//4. 不存在，拦截，返回401状态码
             response.setStatus(401);
-            return false;
+            return false;*/
+            return true;
         }
 
         //2.基于token获取redis中的用户
@@ -56,9 +54,10 @@ public class LoginInterceptor implements HandlerInterceptor {
                 .entries(key);
         //3. 判断用户是否存在
         if(userMap.isEmpty()){
-            //4. 不存在，拦截，返回401状态码
+            /*//4. 不存在，拦截，返回401状态码
             response.setStatus(401);
-            return false;
+            return false;*/
+            return true;
         }
 
         //5. 将查询到的hash数据转化为userDTO对象
@@ -70,16 +69,12 @@ public class LoginInterceptor implements HandlerInterceptor {
         //7. 刷新token有效期
         stringRedisTemplate.expire(key, LOGIN_USER_TTL, TimeUnit.MINUTES);
 
-        return true;*/
-        if(UserHolder.getUser() == null){
-            response.setStatus(401);
-            return false;
-        }
         return true;
+
     }
 
-    /*@Override
+    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
         UserHolder.removeUser();
-    }*/
+    }
 }
